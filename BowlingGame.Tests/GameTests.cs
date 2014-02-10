07196f7d0a8c31db40558357ffa7e29a3b24fdcd
Kevin Poston <kposton@qualtrax.com>
2014-02-10 09:46:13 -0500

    Passed first test.

---
+++ BowlingGame.Tests/GameTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace BowlingGame.Tests
+{
+    [TestFixture]
+    public class GameTests
+    {
+        [Test]
+        public void GutterGame()
+        {
+            var g = new Game();
+
+            for (var i = 0; i < 20; i++)
+                g.Roll(0);
+
+            Assert.That(g.GetScore(), Is.EqualTo(0));
+        }
+    }
+}
---
+++ BowlingGame/Game.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingGame
+{
+    public class Game
+    {
+        public void Roll(int pins)
+        {
+
+        }
+
+        public int GetScore()
+        {
+            return 0;
+        }
+
+    }
+}
